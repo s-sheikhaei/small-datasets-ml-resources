a2897fa534316738426acfc88efb5b85081e1a47
s-sheikhaei <64780392+s-sheikhaei@users.noreply.github.com>
2023-06-16 12:56:02 -0400

    Update train_complete.py
    resolving some issues to make it runnable

--- dl4se/experiments/satd/train_complete.py
+++ dl4se/experiments/satd/train_complete.py
@@ -26,16 +26,18 @@ def main():
         config.lr = 4e-5
         # config.lr = 1e-4
         config.model_type = 'roberta'
-        config.model_path = util.models_path('StackOBERTflow-comments-small-v1')
+        # config.model_path = util.models_path('StackOBERTflow-comments-small-v1')
+        config.model_path = util.models_path('stackoverflow_1M')
         # config.train_head_only = True
 
     ds = TDDataset(config, binary=True)
 
-    tokenizer = tu.load_tokenizer(config)
-    model_cls = tu.get_model_cls(config)
+    model_config = tu.load_model_config(config)
+    tokenizer = tu.load_tokenizer(config, model_config)
+    # model_cls = tu.get_model_cls(config)
 
     train_dataloader = ds.get_complete_train_dataloader(tokenizer)
-    model = tu.load_model(config)
+    model = tu.load_model(config, model_config)
     model.to(config.device)
     util.set_seed(config)
 
